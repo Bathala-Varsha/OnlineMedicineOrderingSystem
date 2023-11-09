907fbd305dec336aadf7a52fcd9a7cf9664bf170
Bathala-Varsha <85429047+Bathala-Varsha@users.noreply.github.com>
2023-11-09 11:33:19 +0530

    Add files via upload

---
+++ DeliveryBoyRepository.java
@@ -0,0 +1,7 @@
+package com.medicine.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.medicine.entity.DeliveryBoy;
+public interface DeliveryBoyRepository extends JpaRepository<DeliveryBoy,String> {
+
+}
